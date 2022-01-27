a3d522c3b97ce1af0ec2d2b4fa35200d7820cb35
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-01-27 14:44:16 -0300

    alterações no background image e adição de algumas paletas secundarias no config.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "025f754e78a69416"; }
+/******/ 		__webpack_require__.h = function() { return "d2e432bc2e3c754d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
